3be2484d205e9b548c1d21bdc786e3087b9edcb8
Snidjers <snidjersgaming@gmail.com>
2018-03-14 14:02:51 +0100

    Data weergeven op de kaart.

--- Source/routes/index.js
+++ Source/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+/* GET map page. */
+router.get('/map', function(req, res, next) {
+  res.render('map', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
