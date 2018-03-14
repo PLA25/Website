8b9fffddab733fb83abb598363e89b2ac9c5c4a2
Martijn Vegter <mail@mvegter.me>
2018-03-14 21:56:27 +0100

    Restored app.js

--- Source/app.js
+++ Source/app.js
@@ -10,6 +10,9 @@ const fs = require('fs');
 const path = require('path');
 const logger = require('morgan');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 const db = mongoose.createConnection('mongodb://admin:admin@ds111279.mlab.com:11279/pla25');
@@ -18,106 +21,79 @@ var dbready = false;
 var connection = null;
 
 db.then(function(conn) {
-	connection = conn;
-	dbready = true;
+  connection = conn;
+  dbready = true;
 });
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname + '/views'));
 
-/**
- *
- * Hier worden alle APIs aan express toegevoegd
- *
- */
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-	secret: 'anfalf4gd(FNvT^^',
-	resave: true,
-	saveUninitialized: false
+  secret: 'anfalf4gd(FNvT^^',
+  resave: true,
+  saveUninitialized: false
 }));
 
 passport.use(new auth({
-	usernameField: 'username',
-	passwordField: 'password',
-	session: true
+  usernameField: 'username',
+  passwordField: 'password',
+  session: true
 }, (un, pw, done) => {
-	if (!dbready) {
-		return done(null, false);
-	}
-	var ready = false;
-	var user = false;
-	connection.collection("users").findOne({"username": un, "password": pw}, function(err, result) {
-		if (!err) {
-			user = result;
-		}
-		ready = true;
-	});
-	while (!ready) {
-		sleep(100);
-	}
-	return done(null, user);
+  if (!dbready) {
+    return done(null, false);
+  }
+  var ready = false;
+  var user = false;
+  connection.collection("users").findOne({
+    "username": un,
+    "password": pw
+  }, function(err, result) {
+    if (!err) {
+      user = result;
+    }
+    ready = true;
+  });
+  while (!ready) {
+    sleep(100);
+  }
+  return done(null, user);
 }));
 
 passport.serializeUser(function(user, done) {
-	done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser(function(user, done) {
-	done(null, user);
+  done(null, user);
 });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-/**
- *
- * Deze constanten worden gebruikt voor:
- *  • root: om het pad naar de map aan te geven
- *
- */
-const root = ({root:__dirname + "\\public"});
+app.use('/', index);
+app.use('/users', users);
 
-/**
- *
- * Hier worden /login, /logout, /map, /admin en /user aangemaakt
- *
- */
-app.use(require('./routes/index'));
-
-/**
- *
- * /user toevoegen
- *
- */
-app.use(require('./routes/users'));
-
-/**
- *
- * de statische map 'public'
- *
- */
-app.use(express.static(path.join(__dirname, "/public")));
-
-/**
- *
- * 404 handling
- *
- */
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
