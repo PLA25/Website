9e3f44a3be673298d8eea3c040a29976506d7544
Martijn Vegter <mail@mvegter.me>
2018-03-14 14:00:41 +0100

    Code Style

--- Source/routes/index.js
+++ Source/routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 /* GET map page. */
 router.get('/map', function(req, res, next) {
-  res.render('map', { title: 'Express' });
+  res.render('map', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
