d68fd3933fcb09ff505f1e9b701947d583b2f094
Martijn Vegter <mail@mvegter.me>
2018-03-17 15:15:51 +0100

    Updated deprecated method

--- Source/routes/api.js
+++ Source/routes/api.js
@@ -4,7 +4,7 @@ var path = require('path');
 var router = express.Router();
 
 router.get('/meetpunten', (req, res) => {
-  res.sendfile(path.join(__dirname, '../public', 'data.kml'));
+  res.sendFile(path.join(__dirname, '../public', 'data.kml'));
 });
 
 router.get('*', (req, res) => {
