dd3ad134a40dc21a76dc82b39df8af727cc04a8e
Martijn Vegter <mail@mvegter.me>
2018-03-17 18:48:13 +0100

    Added API for measurement points

---
+++ Source/routes/api.js
@@ -0,0 +1,24 @@
+var sensorHub = require('./../models/sensorhub');
+var express = require('express');
+var path = require('path');
+
+var router = express.Router();
+
+router.get('/meetpunten', (req, res) => {
+  sensorHub.find({}, function(err, rawHubs) {
+    for (var i = 0; i < rawHubs.length; i++) {
+      rawHubs[i].SerialID = rawHubs[i].SerialID.replace('\n', '');
+    }
+
+    res.render('meetpunten', {
+      layout: false,
+      sensorHubs: rawHubs
+    });
+  });
+});
+
+router.get('*', (req, res) => {
+  res.redirect('/map');
+});
+
+module.exports = router;
