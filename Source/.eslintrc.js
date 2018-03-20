558a00eb4a42c14abcdf29ecb8fbc1932f51bc5d
Martijn Vegter <mail@mvegter.me>
2018-03-20 18:49:46 +0100

    Reworked lines

--- Source/.eslintrc.js
+++ Source/.eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-      "no-param-reassign": "off"
-    }
+  "extends": "airbnb-base",
+  "rules": {
+    'no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxBOF: 0,
+      maxEOF: 1
+    }],
+    "no-param-reassign": "off"
+  }
 };
