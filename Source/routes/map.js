7ba4e12cf6456b8b012016e246073494187a5813
Thom <thom.de.jong3@hva.nl>
2018-03-24 01:49:33 +0100

    D:/Git/map toegevoegd

--- Source/routes/map.js
+++ Source/routes/map.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET map page. */
 router.get('/', (req, res) => {
-  res.render('map', {});
+  res.render('map', {title: "map"});
 });
 
 module.exports = router;
