daaddb9a1116cbe149c6ae4a8ccb57ec830e6e2d
Martijn Vegter <mail@mvegter.me>
2018-03-26 19:05:55 +0200

    Cached Planet Images

--- Source/routes/api.js
+++ Source/routes/api.js
@@ -1,5 +1,8 @@
 const sensorHub = require('./../models/sensorhub');
 const express = require('express');
+const path = require('path');
+const base64 = require('node-base64-image');
+const fs = require('fs');
 
 const router = express.Router();
 
@@ -16,8 +19,38 @@ router.get('/meetpunten', (req, res) => {
   });
 });
 
-router.get('/:z/:x/:y', (req, res) => {
-  res.redirect(`https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2018_02_mosaic/gmap/${req.params.z}/${req.params.x}/${req.params.y}.png?api_key=44db310cea0743da8e73888c2d2d7b3f`);
+router.get('/:z/:x/:y', (req, res, next) => {
+  const cachePath = path.resolve(`${__dirname}./../cache/`);
+  if (!fs.existsSync(cachePath)) {
+    fs.mkdirSync(cachePath);
+  }
+
+  const filePath = path.resolve(cachePath, `${req.params.z}_${req.params.x}_${req.params.y}.jpg`);
+  if (fs.existsSync(filePath)) {
+    res.sendFile(filePath);
+    return;
+  }
+
+  const planetURL = `https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2018_02_mosaic/gmap/${req.params.z}/${req.params.x}/${req.params.y}.png?api_key=44db310cea0743da8e73888c2d2d7b3f`;
+  base64.encode(planetURL, {
+    string: false,
+  }, (err, image) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    base64.decode(image, {
+      filename: filePath.replace('.jpg', ''),
+    }, (err) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      res.sendFile(filePath);
+    });
+  });
 });
 
 router.get('*', (req, res) => {
