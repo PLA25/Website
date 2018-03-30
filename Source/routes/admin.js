3bb2f0a6bff18b82f45f917a7986000ae1a65fbf
Snidjers <Matthijs.snijders@gmail.com>
2018-03-30 13:45:11 +0200

    fixed second table

--- Source/routes/admin.js
+++ Source/routes/admin.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const User = require('./../models/user');
 const SensorHub = require('./../models/sensorhub');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   User.find({}, (err, users) => {
-    res.render('admin', {
-      Users: users,
-    });
-  });
-});
-
-router.get('/meetpunten', (req, res) => {
-  SensorHub.find({}, (err, rawHubs) => {
-    res.render('meetpunten', {
-      layout: false,
-      sensorHubs: rawHubs,
+    SensorHub.find({}, (err, rawHubs) => {
+      res.render('admin', {
+        Users: users,
+        sensorHubs: rawHubs,
+      });
     });
   });
 });
