272f30e135aac4bc8b7b7e4cccf81611913ded42
Snidjers <Matthijs.snijders@gmail.com>
2018-03-30 11:55:45 +0200

    Added second Table

--- Source/routes/admin.js
+++ Source/routes/admin.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const User = require('./../models/user');
+const SensorHub = require('./../models/sensorhub');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   User.find({}, (err, users) => {
     res.render('admin', {
       Users: users,
+    });
+  });
+});
+
+router.get('/meetpunten', (req, res) => {
+  SensorHub.find({}, (err, rawHubs) => {
+    res.render('meetpunten', {
+      layout: false,
+      sensorHubs: rawHubs,
     });
   });
 });
