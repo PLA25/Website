77092825207eb9e321901a08ba04448376731858
Martijn Vegter <mail@mvegter.me>
2018-04-02 12:56:34 +0200

    Moved map script to seperate file

---
+++ Source/public/js/map.js
@@ -0,0 +1,30 @@
+/* global ol */
+
+const center = ol.proj.transform([4.895168, 52.370216], 'EPSG:4326', 'EPSG:3857');
+
+// eslint-disable-next-line no-unused-vars
+const map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: '/api/planet/{z}/{x}/{y}',
+      }),
+    }),
+    new ol.layer.Tile({
+      source: new ol.source.XYZ({
+        url: '/api/mapbox/{z}/{x}/{y}',
+      }),
+    }),
+    new ol.layer.Vector({
+      source: new ol.source.Vector({
+        url: '/api/meetpunten',
+        format: new ol.format.KML(),
+      }),
+    }),
+  ],
+  target: 'map',
+  view: new ol.View({
+    center,
+    zoom: 11,
+  }),
+});
