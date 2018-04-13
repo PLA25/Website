95ae6b544845f38046af4cea944568ad4bf1af10
Martijn Vegter <mail@mvegter.me>
2018-04-13 13:10:04 +0200

    Error Test

--- Source/test/test.js
+++ Source/test/test.js
@@ -3,6 +3,6 @@ const expect = require('expect.js');
 
 describe('tempToColor', () => {
   it('should return true', () => {
-    expect(tempToDegrees(50)).to.equal(0);
+    expect(tempToDegrees(50)).to.equal(10);
   });
 });
