f1bf517580ad6cb4983e6c71ea4d6523f6ff79d4
Martijn Vegter <mail@mvegter.me>
2018-04-13 13:45:34 +0200

    Added more tests

--- Source/test/test.js
+++ Source/test/test.js
@@ -2,7 +2,13 @@ const { tempToDegrees } = require('./../lib/converter');
 const expect = require('expect.js');
 
 describe('tempToColor', () => {
-  it('should return true', () => {
+  it(' 50°C should be equal to   0°', () => {
     expect(tempToDegrees(50)).to.equal(0);
+  });
+  it('-50°C should be equal to 300°', () => {
+    expect(tempToDegrees(-50)).to.equal(300);
+  });
+  it('  0°C should be equal to 150°', () => {
+    expect(tempToDegrees(0)).to.equal(150);
   });
 });
