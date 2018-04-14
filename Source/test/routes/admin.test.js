7360983f8b53f8a602717768e1399d838bdee45d
Martijn Vegter <mail@mvegter.me>
2018-04-14 09:29:16 +0200

    Added admin.test.js

---
+++ Source/test/routes/admin.test.js
@@ -0,0 +1,39 @@
+/* Packages */
+const chai = require('chai');
+const request = require('supertest');
+const app = require('./../../bin/www');
+
+chai.should();
+
+const {
+  authenticatedAdmin,
+  authenticatedUser,
+} = require('./../authenticatedUser');
+
+describe('GET /admin', () => {
+  it('should return a 302 response if the user is logged in', (done) => {
+    authenticatedUser.get('/admin')
+      .end((err, res) => {
+        res.statusCode.should.equal(302);
+        res.headers.location.should.equal('/404');
+        done();
+      });
+  });
+
+  it('should return a 200 response if the admin is logged in', (done) => {
+    authenticatedAdmin.get('/admin')
+      .end((err, res) => {
+        res.statusCode.should.equal(200);
+        done();
+      });
+  });
+
+  it('should return a 302 response and redirect to /login', (done) => {
+    request(app).get('/admin')
+      .end((err, res) => {
+        res.statusCode.should.equal(302);
+        res.headers.location.should.equal('/login');
+        done();
+      });
+  });
+});
