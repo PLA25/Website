6a29ea01cb78a00ad051d1e4d7c56b2d9ecb5207
Martijn Vegter <mail@mvegter.me>
2018-04-16 19:02:34 +0200

    Travis

--- Source/test/lib/helpers.downloadImage.test.js
+++ Source/test/lib/helpers.downloadImage.test.js
@@ -64,8 +64,12 @@ module.exports = () => {
       // Act
       downloadImage(url, imagePath)
         .then((image) => {
+          const expectedImage = path.resolve(`${__dirname}./../expected/mapbox_8_131_84.png`);
+          console.log(expectedImage);
+          console.log(image);
+
           // Assert
-          fs.readFileSync(image).should.deep.equal(fs.readFileSync(path.resolve(`${__dirname}./../expected/mapbox_8_131_84.png`)));
+          fs.readFileSync(image).should.deep.equal(fs.readFileSync(expectedImage));
         });
     });
   });
