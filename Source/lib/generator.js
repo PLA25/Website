6175311f05333816d6d6b09d798fcf5e7136abdf
Martijn Vegter <mail@mvegter.me>
2018-04-25 22:58:21 +0200

    Update generator.js

--- Source/lib/generator.js
+++ Source/lib/generator.js
@@ -3,6 +3,7 @@
 /**
  * @see module:lib
  * @module lib/generator
+ * @todo Documentation
  * @todo Unit Tests
  */
 
@@ -15,15 +16,6 @@ const {
   temperatureToColor,
 } = require('./../helpers/converters');
 
-/**
- * Gets the latitude and longitude of a specific location.
- *
- * @function
- * @param {Number} z - The z coordinate.
- * @param {Number} x - The x coordinate.
- * @param {Number} y - The y coordinate.
- * @returns {Array.<Number>} - returns the latitude and longitude.
- */
 function getLatLong({ z, x, y }) {
   const latitude = tileToLat(parseInt(y, 10), parseInt(z, 10));
   const longitude = tileToLong(parseInt(x, 10), parseInt(z, 10));
@@ -31,16 +23,6 @@ function getLatLong({ z, x, y }) {
   return [latitude, longitude];
 }
 
-/**
- * Gets color from a specific latitude and longitude.
- *
- * @function
- * @param {Number} latitude - The latitude.
- * @param {Number} longitude - The longitude.
- * @param {Object} allSensorHubs - A variable containing all SensorHubs.
- * @param {Object} data - Related data.
- * @returns {Number} - returns the color code.
- */
 function getColorFromLatLong(latitude, longitude, allSensorHubs, data) {
   const to = {
     lat: parseFloat(latitude, 10),
@@ -80,27 +62,10 @@ function getColorFromLatLong(latitude, longitude, allSensorHubs, data) {
   return Jimp.rgbaToInt(rgb[0], rgb[1], rgb[2], parseFloat(0.25 * 255));
 }
 
-/**
- * Gets the increment of a specific z coordinate.
- *
- * @function
- * @param {Number} z - The z coordinate.
- * @returns {Number} - returns the increment.
- */
 function getIncrement(z) {
   return Math.min(2 ** Math.max((15 - parseInt(z, 10)), 3), 128);
 }
 
-/**
- * Generates the map image.
- *
- * @function
- * @param {Object} params - The parameters.
- * @param {Object} allSensorHubs - A variable containing all SensorHubs.
- * @param {data} data - A variable containing all data.
- * @returns {Object} - returns an image of the map.
- * @todo English variable names.
- */
 function generateImage(params, allSensorHubs, data) {
   const [lat1, lon1] = getLatLong(params);
   const [lat2, lon2] = getLatLong({
