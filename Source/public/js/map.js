fe7aa974403b3f129002036c24753735d890c9f7
Martijn Vegter <martijn@martijnvegter.com>
2018-04-26 09:39:31 +0200

    Feature - Sidebar (#42)

--- Source/public/js/map.js
+++ Source/public/js/map.js
@@ -1,45 +1,102 @@
 /* global ol */
+$(document).ready(() => {
+  /* Layers */
+  const googleLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: 'http://mt{0-3}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+    }),
+  });
 
-const center = ol.proj.transform([4.895168, 52.370216], 'EPSG:4326', 'EPSG:3857');
+  const heatmapLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: '/api/heatmap/{z}/{x}/{y}',
+    }),
+  });
 
-const view = new ol.View({
-  center,
-  zoom: 8,
-  minZoom: 8,
-  maxZoom: 15,
-  extent: [
-    375000, // Left
-    6580000, // Bottom
-    800000, // Right
-    7075000, // Top
-  ],
-});
+  const mapboxLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: '/api/mapbox/{z}/{x}/{y}',
+    }),
+  });
 
-// eslint-disable-next-line no-unused-vars
-const map = new ol.Map({
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.XYZ({
-        url: '/api/planet/{z}/{x}/{y}',
-      }),
+  const planetLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: '/api/planet/{z}/{x}/{y}',
     }),
-    new ol.layer.Tile({
-      source: new ol.source.XYZ({
-        url: '/api/mapbox/{z}/{x}/{y}',
-      }),
+  });
+
+  const sensorhubLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      url: '/api/sensorhubs',
+      format: new ol.format.KML(),
     }),
-    new ol.layer.Vector({
-      source: new ol.source.Vector({
-        url: '/api/sensorhubs',
-        format: new ol.format.KML(),
-      }),
-    }),
-    new ol.layer.Tile({
-      source: new ol.source.XYZ({
-        url: '/api/heatmap/{z}/{x}/{y}',
-      }),
-    }),
-  ],
-  target: 'map',
-  view,
+  });
+
+  const center = ol.proj.transform([4.895168, 52.370216], 'EPSG:4326', 'EPSG:3857');
+  const view = new ol.View({
+    center,
+    zoom: 8,
+    minZoom: 8,
+    maxZoom: 15,
+    extent: [
+      375000, // Left
+      6580000, // Bottom
+      800000, // Right
+      7075000, // Top
+    ],
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  const map = new ol.Map({
+    layers: [
+      googleLayer,
+      planetLayer,
+      mapboxLayer,
+      sensorhubLayer,
+      heatmapLayer,
+    ],
+    target: 'map',
+    view,
+  });
+
+  /* Initial state of the checkboxes */
+  $('#mapboxLayer').prop('checked', true);
+  $('#heatmapLayer').prop('checked', true);
+  $('#sensorhubLayer').prop('checked', true);
+
+  /* Events */
+  $('#heatmapLayer').change(() => {
+    const isChecked = $('#heatmapLayer').prop('checked');
+    heatmapLayer.setVisible(isChecked);
+  });
+
+  $('#mapboxLayer').change(() => {
+    const isChecked = $('#mapboxLayer').prop('checked');
+    mapboxLayer.setVisible(isChecked);
+  });
+
+  $('#sensorhubLayer').change(() => {
+    const isChecked = $('#sensorhubLayer').prop('checked');
+    sensorhubLayer.setVisible(isChecked);
+  });
+
+  $('#typeSat').change(() => {
+    const isChecked = $('#typeSat').prop('checked');
+    googleLayer.setVisible(!isChecked);
+    planetLayer.setVisible(isChecked);
+
+    $('#mapboxLayer').prop('checked', true);
+    $('#mapboxLayer').prop('disabled', false);
+    mapboxLayer.setVisible(true);
+  });
+
+  $('#typeMap').change(() => {
+    const isChecked = $('#typeMap').prop('checked');
+    googleLayer.setVisible(isChecked);
+    planetLayer.setVisible(!isChecked);
+
+    $('#mapboxLayer').prop('checked', false);
+    $('#mapboxLayer').prop('disabled', true);
+    mapboxLayer.setVisible(false);
+  });
 });
