8e643a32d1b7e458062983da6a6e8fb89b170381
Joey Blankendaal <joeyblankendaal@gmail.com>
2018-04-29 14:13:09 +0200

    Fixed

--- Source/controllers/home.controller.js
+++ Source/controllers/home.controller.js
@@ -87,15 +87,5 @@ router.get('/map', isLoggedIn, (req, res) => {
   res.render('map');
 });
 
-/**
- * Renders the 404 page.
- *
- * @name 404
- * @path {GET} /404
- */
-router.get('/404', isLoggedIn, (req, res) => {
-  res.render('404');
-});
-
 /* Exports */
 module.exports = router;
