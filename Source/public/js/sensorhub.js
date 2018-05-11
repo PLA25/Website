830c1adab652fff0a6d18d54796d60be73fdaaa2
Snidjers <Matthijs.snijders@gmail.com>
2018-05-11 21:55:58 +0200

    datatable change

---
+++ Source/public/js/sensorhub.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  $('#table-sensorhubs').DataTable({
+    columnDefs: [{
+      orderable: false,
+      searchable: false,
+      targets: [3, 4, 5],
+    }],
+  });
+});
