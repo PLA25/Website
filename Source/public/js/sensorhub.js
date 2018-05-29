680ebb300562d55595e0ea024d2b7f18a5f98408
Martijn Vegter <mail@mvegter.me>
2018-05-29 19:17:44 +0200

    Added flip to light and gass

--- Source/public/js/sensorhub.js
+++ Source/public/js/sensorhub.js
@@ -6,6 +6,18 @@ $(document).ready(() => {
       targets: [2],
     }],
   });
-  $('#table-light').DataTable();
-  $('#table-gass').DataTable();
+  $('#table-light').DataTable({
+    columnDefs: [{
+      orderable: false,
+      searchable: false,
+      targets: [2],
+    }],
+  });
+  $('#table-gass').DataTable({
+    columnDefs: [{
+      orderable: false,
+      searchable: false,
+      targets: [2],
+    }],
+  });
 });
