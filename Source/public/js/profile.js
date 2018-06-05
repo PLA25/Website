3638a7fbfa48155c8356735764f4a1b948d945ed
ThomAat2 <thomdejong99@hotmail.nl>
2018-06-05 23:43:07 +0200

    ander soort fix

--- Source/public/js/profile.js
+++ Source/public/js/profile.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-  const knop = $('button.btn.btn-warning');
+  const knop = $('button.btn#edit-button');
 
   knop.on('click', () => {
     window.location.href = '/account/edit';
