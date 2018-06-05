9c2f0cc1dc9e79a79fa5e39ae5da942c3f00d8ba
ThomAat2 <thomdejong99@hotmail.nl>
2018-06-05 16:25:09 +0200

    Tests nu werkend

--- Source/test/controllers/account.controller.test.js
+++ Source/test/controllers/account.controller.test.js
@@ -1,16 +1,15 @@
 /* Packages */
 const chai = require('chai');
-//const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcrypt-nodejs');
 const request = require('supertest');
 const app = require('./../../bin/www');
-// const User = require('./../../models/user');
-
-// const salt = bcrypt.genSaltSync(8);
+const User = require('./../../models/user');
 
 /* Constants */
 const {
   authenticatedAdmin,
   authenticatedUser,
+  authenticatedTestUser,
 } = require('./../authenticatedUser')();
 
 chai.should();
@@ -51,38 +50,44 @@ module.exports = () => {
     });
   });
 
-  /*
-   * describe('POST /account', () => {
-   * describe('password', () => {
-   * it('should change the password', (done) => {
-   * authenticatedUser
-   * .post('/account/edit')
-   * .send({oldPass: "user", newPass: "User", repeatPass: "User", passChange: true})
-   * .end((err, res) => {
-   * User.findOne({email: "user"}).exec().then((user) => {
-   * if (user) {
-   * var pw = bcrypt.hashSync(user.password, salt);
-   * pw.should.equal('User');
-   * }
-   * done();
-   * });
-   * });
-   * });
-   *});
-   */
+  describe('POST /account', () => {
+    describe('password', () => {
+      it('should change the password', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            oldPass: 'test',
+            newPass: 'Test',
+            repeatPass: 'Test',
+            passChange: true,
+          })
+          .end(() => {
+            User.findOne({
+              email: 'test',
+            }).exec().then((user) => {
+              if (user) {
+                const succeeded = (bcrypt.compareSync('Test', user.password)) ? 'OK' : 'ERROR';
+                succeeded.should.equal('OK');
+                // eslint-disable-next-line no-param-reassign
+                user.password = bcrypt.hashSync('test', bcrypt.genSaltSync(8));
+                user.save();
+              }
+              done();
+            });
+          });
+      });
+    });
 
-  /*
-   * describe('incorrect password', () => {
-   * it('should redirect to /account/edit?fail=true', (done) => {
-   * authenticatedUser
-   * .post('/account/edit')
-   * .send()
-   * .end((err, res) => {
-   * res.headers.location.should.equal('/account/edit');
-   * done();
-   * })
-   * })
-   * });
-   *});
-   */
+    describe('incorrect password', () => {
+      it('should redirect to /account/edit?fail=true', (done) => {
+        authenticatedUser
+          .post('/account/edit')
+          .send({ nameChange: true })
+          .end((err, res) => {
+            res.headers.location.should.equal('/account/edit?fail=true');
+            done();
+          });
+      });
+    });
+  });
 };
