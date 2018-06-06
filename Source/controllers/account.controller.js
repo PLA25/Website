d6a7d1b744783ff406f291bcd4bb204e832f548c
ThomAat2 <36235852+ThomAat2@users.noreply.github.com>
2018-06-06 14:41:14 +0200

    Feature - Profile page (#72)

---
+++ Source/controllers/account.controller.js
@@ -0,0 +1,95 @@
+/**
+ * Account controller
+ *
+ * @module controllers/account
+ * @see module:controllers
+ */
+
+/* Packages */
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const User = require('./../models/user');
+
+/* Constants */
+const router = express.Router();
+const salt = bcrypt.genSaltSync(8);
+
+/* Middlewares */
+const {
+  isLoggedIn,
+} = require('./../middlewares');
+
+/**
+ * Renders the profile page.
+ *
+ * @name Profile
+ * @path {GET} /account
+ */
+router.get('/', isLoggedIn, (req, res) => {
+  res.render('profile', {
+    title: 'Profile Page',
+  });
+});
+
+/**
+ * Saves the changes to an account
+ *
+ * @name Profile
+ * @path {POST} /account/edit
+ */
+router.post('/edit', isLoggedIn, (req, res, next) => {
+  const {
+    passChange,
+    oldPass,
+    newPass,
+    repeatPass,
+    nameChange,
+    name,
+  } = req.body;
+  User.findOne({
+    email: req.user.email,
+  }).exec().then((user) => {
+    if (!user) {
+      next(new Error(`Could not find user with email: '${req.user.email}'!`));
+      return;
+    }
+    if (passChange && (!!oldPass && !!newPass && !!repeatPass) && newPass === repeatPass) {
+      if (user.validatePassword(oldPass)) {
+        // eslint-disable-next-line no-param-reassign
+        user.password = bcrypt.hashSync(
+          newPass,
+          salt,
+        );
+        user.save();
+        res.redirect('/logout');
+      } else {
+        res.redirect('/account/edit?fail=true');
+      }
+    } else if (nameChange && name) {
+      // eslint-disable-next-line no-param-reassign
+      user.name = name;
+      user.save();
+      res.redirect('/account');
+    } else {
+      res.redirect('/account/edit?fail=true');
+    }
+  }).catch((err) => {
+    next(err);
+  });
+});
+
+/**
+ * The account edit page
+ *
+ * @name Profile
+ * @path {GET} /account/edit
+ */
+router.get('/edit', isLoggedIn, (req, res) => {
+  res.render('editProfile', {
+    title: 'Edit Account',
+    fail: (req.query.fail === 'true'),
+  });
+});
+
+/* Exports */
+module.exports = router;
---
+++ Source/test/controllers/account.controller.test.js
@@ -0,0 +1,181 @@
+/* Packages */
+const chai = require('chai');
+const bcrypt = require('bcrypt-nodejs');
+const request = require('supertest');
+const app = require('./../../bin/www');
+const User = require('./../../models/user');
+
+/* Constants */
+const {
+  authenticatedAdmin,
+  authenticatedUser,
+  authenticatedTestUser,
+} = require('./../authenticatedUser')();
+
+chai.should();
+
+module.exports = () => {
+  describe('GET /account/edit', () => {
+    describe('Not logged in', () => {
+      it('should redirect to /login', (done) => {
+        request(app)
+          .get('/account/edit')
+          .end((err, res) => {
+            res.headers.location.should.equal('/login');
+            done();
+          });
+      });
+    });
+
+    describe('Logged in admin', () => {
+      it('should return a 200 response', (done) => {
+        authenticatedAdmin
+          .get('/account/edit')
+          .end((err, res) => {
+            res.statusCode.should.equal(200);
+            done();
+          });
+      });
+    });
+
+    describe('Logged in user', () => {
+      it('should return a 200 response', (done) => {
+        authenticatedUser
+          .get('/account/edit')
+          .end((err, res) => {
+            res.statusCode.should.equal(200);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('GET /account', () => {
+    describe('Not logged in', () => {
+      it('should redirect to /login', (done) => {
+        request(app)
+          .get('/account')
+          .end((err, res) => {
+            res.headers.location.should.equal('/login');
+            done();
+          });
+      });
+    });
+
+    describe('Logged in admin', () => {
+      it('should return a 200 response', (done) => {
+        authenticatedAdmin
+          .get('/account')
+          .end((err, res) => {
+            res.statusCode.should.equal(200);
+            done();
+          });
+      });
+    });
+
+    describe('Logged in user', () => {
+      it('should return a 200 response', (done) => {
+        authenticatedUser
+          .get('/account')
+          .end((err, res) => {
+            res.statusCode.should.equal(200);
+            done();
+          });
+      });
+    });
+  });
+
+  describe('POST /account/edit', () => {
+    describe('Password change', () => {
+      it('should change the password', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            oldPass: 'test',
+            newPass: 'Test',
+            repeatPass: 'Test',
+            passChange: true,
+          })
+          .end(() => {
+            User.findOne({
+              email: 'test',
+            }).exec().then((user) => {
+              if (user) {
+                chai.expect(bcrypt.compareSync('Test', user.password)).to.equal(true);
+                // eslint-disable-next-line no-param-reassign
+                user.password = bcrypt.hashSync('test', bcrypt.genSaltSync(8));
+                user.save();
+              }
+              done();
+            }).catch(() => {
+              chai.assert.fail();
+              done();
+            });
+          });
+      });
+    });
+
+    describe('Incorrect password', () => {
+      it('should redirect to /account/edit?fail=true', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            oldPass: 'Oepsie',
+            newPass: 'Test',
+            repeatPass: 'Test',
+            passChange: true,
+          })
+          .end((err, res) => {
+            res.headers.location.should.equal('/account/edit?fail=true');
+            done();
+          });
+      });
+    });
+
+    describe('Repeat password does not match', () => {
+      it('should redirect to /account/edit?fail=true', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            oldPass: 'test',
+            newPass: 'Test',
+            repeatPass: 'Testt',
+            passChange: true,
+          })
+          .end((err, res) => {
+            res.headers.location.should.equal('/account/edit?fail=true');
+            done();
+          });
+      });
+    });
+
+    describe('No name specified', () => {
+      it('should redirect to /account/edit?fail=true', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            nameChange: true,
+          })
+          .end((err, res) => {
+            res.headers.location.should.equal('/account/edit?fail=true');
+            done();
+          });
+      });
+    });
+
+    describe('Name change succeeded', () => {
+      it('should redirect to /account/edit?fail=true', (done) => {
+        authenticatedTestUser
+          .post('/account/edit')
+          .send({
+            name: 'PDS Test User',
+            nameChange: true,
+          })
+          .end((err, res) => {
+            res.headers.location.should.equal('/account');
+            done();
+          });
+      });
+    });
+  });
+};
